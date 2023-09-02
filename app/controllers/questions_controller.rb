2f8b252e6710f410f327d87ab244d6821a6e1760
yoshin04 <taigaby0411@gmail.com>
2023-09-02 15:02:06 +0900

    質問一覧、作成、詳細実装

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,47 @@
+class QuestionsController < ApplicationController
+  # 質問一覧表示
+  def index
+    @questions = Question.all
+    p @questions
+  end
+
+  # 質問詳細ページ表示
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  # 質問の作成
+  def new
+    @question = Question.new
+  end
+
+  # 質問の登録
+  def create
+    # Questionモデルを初期化
+    @question = Question.new(question_params)
+    # QuestionモデルをDBへ保存
+    if @question.save
+    # showへリダイレクト
+    redirect_to @question
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  # 質問の編集
+  def edit
+  end
+
+  # 質問の更新
+  def update
+  end
+
+  # 質問の削除
+  def destroy
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :name, :content)
+  end
+end
